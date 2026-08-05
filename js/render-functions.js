8749af44d718eb8ffe0171d69305541049b53dcb
leontievasophia <leontievasophia@gmail.com>
2026-08-05 16:24:24 +0300

    Fix DOM selectors and checks

---
+++ js/render-functions.js
@@ -0,0 +1,68 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const loadMore = document.querySelector('.load-btn');
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+export function createGallery(images) {
+  if (!gallery) return;
+
+  const markup = images
+    .map(image => {
+      return `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${image.largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${image.webformatURL}"
+              alt="${image.tags}"
+            />
+          </a>
+
+          <div class="image-info">
+            <p><b>Likes</b>${image.likes}</p>
+            <p><b>Views</b>${image.views}</p>
+            <p><b>Comments</b>${image.comments}</p>
+            <p><b>Downloads</b>${image.downloads}</p>
+          </div>
+        </li>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  if (!gallery) return;
+
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  if (!loader) return;
+
+  loader.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  if (!loader) return;
+
+  loader.classList.add('is-hidden');
+}
+
+export function showLoadMoreButton() {
+  if (!loadMore) return;
+
+  loadMore.classList.remove('is-hidden');
+}
+
+export function hideLoadMoreButton() {
+  if (!loadMore) return;
+
+  loadMore.classList.add('is-hidden');
+}
