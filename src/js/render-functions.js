b344fb403ef34a3cc6187fcc927f644fa931ff89
leontievasophia <leontievasophia@gmail.com>
2026-08-05 16:18:58 +0300

    Move js files into src

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,7 +4,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
-const loadMore= docement.querySelector('load-btn');
+const loadMore= docement.querySelector('.load-btn');
 
 const lightbox = new SimpleLightbox('.gallery a');
 
